5592aefc2df025312b34e713c1ed7a3173c4d707
Matt <mattkchao@gmail.com>
2023-01-21 10:34:58 -0800

    deleted linux file

--- Linux/05.02/lynis.partial.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# run daily scans for thetest groups: malware, authentication, networking, storage, and
-# filesystems, and saves the results in /tmp/lynis.partial_scan.log
-lynis audit --tests-from-group malware,authentication,networking,storage,filesystems >> /tmp/lynis.partial_scan.log
